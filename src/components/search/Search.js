a46014142c851eb5dfb7d393bffdb809625b2e51
N1ghting4le <nikansoloveib@gmail.com>
2023-07-14 20:02:22 +0300

    search icon added

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -6,7 +6,10 @@ const Search = ({setTown}) => {
     }
 
     return (
-        <input type="text" placeholder="Enter town name..." onKeyDown={updateTown}/>
+        <div style={{'position': 'relative'}}>
+            <input type="text" placeholder="Enter town name..." onKeyDown={updateTown}/>
+            <img src="https://img.icons8.com/ios/50/search.png" alt="search icon" className="search_icon" onClick={(e) => setTown(e.target.previousElementSibling.value)}/>
+        </div>
     );
 };
 
