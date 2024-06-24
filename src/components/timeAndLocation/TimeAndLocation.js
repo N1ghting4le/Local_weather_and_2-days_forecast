5448e894ecfa6e2813aa3ddff3d826e61cc24d49
N1ghting4le <nikansoloveib@gmail.com>
2024-06-24 16:27:23 +0300

    mobile adaptivity issue is fixed

--- src/components/timeAndLocation/TimeAndLocation.js
+++ src/components/timeAndLocation/TimeAndLocation.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import './timeAndLocation.css';
 
 const Arrow = ({classNames, handleClick}) => (
-    <i className={'arrow ' + classNames}
+    <i className={`arrow ${classNames}`}
         tabIndex={0}
         onClick={handleClick}
         onKeyDown={(e) => onEnter(e, handleClick)}/>
@@ -14,18 +14,18 @@ const TimeAndLocation = () => {
     const {process, locationAndTime, currentLocationAndTime, setlocationAndTime, dailyForecasts, num, current, setWeather, updateWeather} = useGlobalContext();
     const currHours = getHours(currentLocationAndTime);
     const hours = getHours(locationAndTime);
-    const disableDecreaseCondition = (num === 0 && hours === currHours) || hours === 0;
-    const disableIncreaseCondition = hours === 23;
+    const disableDecrease = (num === 0 && hours === currHours) || hours === 0;
+    const disableIncrease = hours === 23;
 
     const onHoursIncrease = () => {
-        if (disableIncreaseCondition) return;
+        if (disableIncrease) return;
 
         setlocationAndTime(state => ({...state, time: `${hours + 1}:00`}));
         updateWeather(dailyForecasts[num], hours + 1);
     }
 
     const onHoursDecrease = () => {
-        if (disableDecreaseCondition) return;
+        if (disableDecrease) return;
 
         if (num === 0 && hours - 1 === currHours) {
             setlocationAndTime(currentLocationAndTime);
@@ -45,10 +45,10 @@ const TimeAndLocation = () => {
             <span style={{textAlign: 'center'}}>{locationAndTime.place}, {locationAndTime.country}</span>
             <span>{locationAndTime.date}</span>
             <span className="time">
-                <Arrow classNames={`left ${toggleArrow(disableDecreaseCondition)}`}
+                <Arrow classNames={`left ${toggleArrow(disableDecrease)}`}
                         handleClick={onHoursDecrease}/>
                 {locationAndTime.time}
-                <Arrow classNames={`right ${toggleArrow(disableIncreaseCondition)}`}
+                <Arrow classNames={`right ${toggleArrow(disableIncrease)}`}
                         handleClick={onHoursIncrease}/>
             </span>
         </div>
